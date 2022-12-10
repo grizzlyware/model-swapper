c7a9ac311fed4e35e1800e8fb0932f70ed171cd9
mewejo <mewejo@users.noreply.github.com>
2022-12-10 20:12:35 +0000

    Fix styling

--- tests/Resources/ReplacementModels/Country.php
+++ tests/Resources/ReplacementModels/Country.php
@@ -4,7 +4,6 @@ namespace Grizzlyware\ModelSwapper\Tests\Resources\ReplacementModels;
 
 use Grizzlyware\ModelSwapper\Tests\Resources\Models\Country as OriginalCountry;
 use Grizzlyware\ModelSwapper\Traits\IsReplacementModel;
-use Illuminate\Database\Eloquent\Model;
 
 class Country extends OriginalCountry
 {

0f3734ac3175820303bf1f64b0a1879f7f05b675
Josh Bonfield <josh@grizzlyware.com>
2024-03-04 14:19:17 +0000

    feat: handle model events

--- src/Traits/IsReplacementModel.php
+++ src/Traits/IsReplacementModel.php
@@ -86,4 +86,31 @@ trait IsReplacementModel
             ->getTable()
         ;
     }
+
+    public static function observe($class): void
+    {
+        $replacedModel = (new static())->getReplacedModelInstance();
+        
+        $replacedModel::observe($class);
+    }
+
+    protected function fireModelEvent($event, $halt = true)
+    {
+        // First, we will get the proper method to call on the event dispatcher, and then we
+        // will attempt to fire a custom, object based event for the given event. If that
+        // returns a result we can return that result, or we'll call the string events.
+        $method = $halt ? 'until' : 'dispatch';
+
+        $result = $this->filterModelEventResults(
+            $this->fireCustomModelEvent($event, $method)
+        );
+
+        if ($result === false) {
+            return false;
+        }
+
+        return ! empty($result) ? $result : static::$dispatcher->{$method}(
+            "eloquent.{$event}: " . \get_parent_class($this), $this
+        );
+    }
 }
